dbc2ffd3996a02fadc0ceb72f4ed83b8793566e0
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-03-22 21:34:34 +0100

    Ваш коментар до коміту тут

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -8,9 +8,10 @@ const Main = () => {
       <div className={css.main}>
         <h2 className={css.title}>Вирішуєте з чого почати?</h2>
         <p className={css.text1}>Найкращі цінові пропозиції</p>
+    
         <p>саме для вас!</p>
         <button className={css.button}>Хочу перевірити :)</button>
-        <button className={css.button}>Хочу перевірити :)</button>
+ 
       </div>
     </section>
   );
