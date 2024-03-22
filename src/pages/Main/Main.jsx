5b830bc6b3e111a24fb22cacde3df26e946ec051
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-03-22 20:35:50 +0100

    -

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
         <h2 className={css.title}>Вирішуєте з чого почати?</h2>
         <p className={css.text1}>Найкращі цінові пропозиції</p>
         <p>саме для вас!</p>
-        <button>Хочу перевірити :)</button>
+        <button className={css.button}>Хочу перевірити :)</button>
       </div>
     </section>
   );
