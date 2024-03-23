bd95957cced809c6d6c66145fe0d0fc7b5473b1c
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-03-23 11:30:59 +0100

    Ваш коментар до коміту тут

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import css from './Footer.module.css';
 import logoFooter from '../../assets/img/logo-footer.png';
+import mastercard from '../../assets/img/mastercard.png';
+import liqpay from '../../assets/img/liqpay.png';
+import visa from '../../assets/img/visa.png';
 const Footer = () => {
     return (
       <footer className={css.footer}>
@@ -10,6 +13,34 @@ const Footer = () => {
             © 2013 - 2024 AQUAPRO. Всі права захищені Разработка сайта
             WellDigital
           </p>
+        </div>
+        <div className={css.menuFooter}>
+          <p className={css.menuText}>Головна</p>
+          <p className={css.menuText}>Про воду</p>
+          <p className={css.menuText}>Доставка і ціни</p>
+          <p className={css.menuText}>Акції</p>
+          <p className={css.menuText}>Контакти</p>
+          <p className={css.menuText}>Договір-оферта</p>
+        </div>
+        <div className={css.payments}>
+          <p className={css.paymentsText}>Способи оплати:</p>
+          <div className={css.imgMenu}>
+            <img src={mastercard} alt="Mastercard" width="80px" height="47px" />
+            <img
+              className={css.imgLiqpay}
+              src={liqpay}
+              alt="Liqpay"
+              width="80px"
+              height="16px"
+            />
+            <img
+              className={css.imgVisa}
+              src={visa}
+              alt="Visa"
+              width="80px"
+              height="32px"
+            />
+          </div>
         </div>
       </footer>
     );
